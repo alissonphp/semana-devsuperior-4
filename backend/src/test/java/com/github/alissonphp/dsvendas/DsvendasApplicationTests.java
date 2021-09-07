a26a0a173196cf8074450da364f580a75dc680a5
Alisson Gomes <alisson.php@gmail.com>
2021-09-07 09:51:29 -0300

    chore(init): Initial commit

---
+++ backend/src/test/java/com/github/alissonphp/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alissonphp.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
